3d86150cb4f6aa8c76d783206af615cab71f8022
ryderwei50505 <ghetto50505@gmail.com>
2025-07-15 17:01:37 +0800

    ts

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,12 @@
 import { cn } from "@/lib/utils";
-export function Button({ children, className, ...props }) {
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+
+interface ButtonProps
+  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  className?: string;
+}
+
+export function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button className={cn("px-4 py-2 rounded border", className)} {...props}>
       {children}
